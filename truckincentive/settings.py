0517939f5a49feee5995198dec698dc952b9bbba
Jacob Roberts <154029480+slothsage@users.noreply.github.com>
2025-09-23 12:36:02 -0400

    Update settings.py

--- truckincentive/settings.py
+++ truckincentive/settings.py
@@ -71,7 +71,7 @@ PASSWORD_HASHERS = [
 
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", "OPTIONS": {"min_length": 8}},
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", "OPTIONS": {"min_length": 12}},
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},   
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
     {"NAME": "accounts.validators.PolicyComplexityValidator"},
