696e24074b75890384023aa784b9a1669ac5a720
Jacob Roberts <slothsage@proton.me>
2025-10-05 23:07:07 -0400

    Update settings.py

--- truckincentive/settings.py
+++ truckincentive/settings.py
@@ -85,7 +85,10 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 # STATICFILES_DIRS = [BASE_DIR / "static"]
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
